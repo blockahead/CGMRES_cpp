10356bb6c825dd44fce18add056447faf5efc555
Hamada_Kwansei <hamadaka@kwansei.ac.jp>
2021-03-27 03:11:22 +0900

    Delete build directory.

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/hamada/Documents/cpp/CGMRES_cpp
-# Build directory: /home/hamada/Documents/cpp/CGMRES_cpp/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
